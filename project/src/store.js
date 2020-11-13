572a3a04416d7a676abf99a73dda81d191522275
Stepan-a11y <kostko52@gmail.com>
2020-11-13 09:03:12 +0300

    add code

---
+++ project/src/store.js
@@ -0,0 +1,21 @@
+let state = {
+     orders: [
+         { id: 1, nameMaster: "Dmitry Sokolow", dateOrder: "12 september 2020 - 12:20"},
+         { id: 2, nameMaster: "Petr Ivanow", dateOrder: "23 september 2020 - 14:40"}
+     ]
+}
+
+
+
+const addOrder = () => {
+
+}
+
+
+const updateInform = () => {
+
+}
+
+
+
+export default state;
